7ca951a6e6330d36daf30eb4dad7bb8d5cc50a34
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2024-05-23 15:25:40 -0400

    Move modules to live and modules folders

--- ch2/tofu/live/sample-app/main.tf
+++ ch2/tofu/live/sample-app/main.tf
@@ -3,8 +3,7 @@ provider "aws" {
 }
 
 module "sample_app_1" {
-
-  source = "../ec2-instance-module"
+  source = "../../modules/ec2-instance"
 
   # TODO: fill in with your own AMI ID!
   ami_id = "ami-09a9ad4735def0515"
@@ -13,8 +12,7 @@ module "sample_app_1" {
 }
 
 module "sample_app_2" {
-
-  source = "../ec2-instance-module"
+  source = "../../modules/ec2-instance"
 
   # TODO: fill in with your own AMI ID!
   ami_id = "ami-09a9ad4735def0515"
---
+++ ch2/tofu/live/sample-app/outputs.tf
@@ -0,0 +1,15 @@
+output "sample_app_1_public_ip" {
+  value = module.sample_app_1.public_ip
+}
+
+output "sample_app_2_public_ip" {
+  value = module.sample_app_2.public_ip
+}
+
+output "sample_app_1_instance_id" {
+  value = module.sample_app_1.instance_id
+}
+
+output "sample_app_2_instance_id" {
+  value = module.sample_app_2.instance_id
+}
