1fd5f5d43dca0a3593f67880fd17d487da10dfb4
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2024-06-20 15:41:53 -0400

    Update chapter 2 bash code

--- ch2/bash/deploy-ec2-instance.sh
+++ ch2/bash/deploy-ec2-instance.sh
@@ -4,7 +4,7 @@ set -e
 
 export AWS_DEFAULT_REGION="us-east-2"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-user_data=$(cat "$SCRIPT_DIR/../../ch/ec2-user-data-script/user-data.sh")
+user_data=$(cat "$SCRIPT_DIR/user-data.sh")
 
 security_group_id=$(aws ec2 create-security-group \
   --group-name "sample-app" \
---
+++ ch2/bash/user-data.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+curl -fsSL https://rpm.nodesource.com/setup_21.x | bash -
+yum install -y nodejs
+
+tee app.js > /dev/null << "EOF"
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
+});
+
+const port = 80;
+server.listen(port,() => {
+  console.log(`Listening on port ${port}`);
+});
+EOF
+
+nohup node app.js &
