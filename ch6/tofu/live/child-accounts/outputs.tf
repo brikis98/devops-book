e8c7fa7b712f28cca0917647d5610f54357b5576
Yevgeniy Brikman <brikis98@users.noreply.github.com>
2024-07-18 15:22:12 +0100

    Shorten output var names more

--- ch6/tofu/live/child-accounts/outputs.tf
+++ ch6/tofu/live/child-accounts/outputs.tf
@@ -14,17 +14,17 @@ output "prod_account_id" {
   value       = module.child_accounts.prod_account_id
 }
 
-output "dev_iam_role_arn" {
+output "dev_role_arn" {
   description = "The ARN of the IAM role you can use to manage dev from mgmt"
-  value       = module.child_accounts.dev_iam_role_arn
+  value       = module.child_accounts.dev_role_arn
 }
 
-output "stage_iam_role_arn" {
+output "stage_role_arn" {
   description = "The ARN of the IAM role you can use to manage stage from mgmt"
-  value       = module.child_accounts.stage_iam_role_arn
+  value       = module.child_accounts.stage_role_arn
 }
 
-output "prod_iam_role_arn" {
+output "prod_role_arn" {
   description = "The ARN of the IAM role you can use to manage prod from mgmt"
-  value       = module.child_accounts.prod_iam_role_arn
+  value       = module.child_accounts.prod_role_arn
 }
