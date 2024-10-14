6e66619ae734c2da3d07cffe76ff32db2cb08242
bulma1 <lyruz2354@gmail.com>
2024-10-14 09:41:10 +0700

    Change response text

--- ch5/sample-app/app.js
+++ ch5/sample-app/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Fundamentals of Devops!');
+  res.send('Fundamentals of Devops!!');
 });
 
 module.exports = app;
